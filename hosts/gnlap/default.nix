10f5c43322178f9cad6aa1d64b01d681479d614d
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-05-21 15:53:05 -0500

    remove hardware

--- hosts/gnlap/default.nix
+++ hosts/gnlap/default.nix
@@ -3,8 +3,7 @@
 {
   imports =                                     # For now, if applying to other system, swap files
     [(import ./hardware-configuration.nix)] ++            # Current system hardware config @ /etc/nixos/hardware-configuration.nix
-    [(import ../../modules/gnlap/qtile/qtile.nix)] ++   # Window Manager
-    (import ../../modules/hardware);                      # Hardware devices
+    [(import ../../modules/gnlap/qtile/qtile.nix)];       # Window Manager
 
   boot = {                                      # Boot options
     kernelPackages = pkgs.linuxPackages_latest;
